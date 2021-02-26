a5d926ede9c83273ad3e3d701aa2e32289e8918c
fmarcos <fmarcosdev@gmail.com>
2021-02-25 21:20:19 -0300

    cambios de cotenido

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,15 +5,15 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/fmarcos/Desktop/apps/running/Proyecto-FmarcosDevBlogCV/.cache/caches/gatsby-plugin-offline/app-shell.js"))),
-  "component---src-pages-404-index-jsx": hot(preferDefault(require("/Users/fmarcos/Desktop/apps/running/Proyecto-FmarcosDevBlogCV/src/pages/404/index.jsx"))),
-  "component---src-pages-blog-index-jsx": hot(preferDefault(require("/Users/fmarcos/Desktop/apps/running/Proyecto-FmarcosDevBlogCV/src/pages/blog/index.jsx"))),
-  "component---src-pages-contact-index-jsx": hot(preferDefault(require("/Users/fmarcos/Desktop/apps/running/Proyecto-FmarcosDevBlogCV/src/pages/contact/index.jsx"))),
-  "component---src-pages-index-jsx": hot(preferDefault(require("/Users/fmarcos/Desktop/apps/running/Proyecto-FmarcosDevBlogCV/src/pages/index.jsx"))),
-  "component---src-pages-portfolio-index-jsx": hot(preferDefault(require("/Users/fmarcos/Desktop/apps/running/Proyecto-FmarcosDevBlogCV/src/pages/portfolio/index.jsx"))),
-  "component---src-pages-resume-jsx": hot(preferDefault(require("/Users/fmarcos/Desktop/apps/running/Proyecto-FmarcosDevBlogCV/src/pages/resume.jsx"))),
-  "component---src-pages-tags-index-jsx": hot(preferDefault(require("/Users/fmarcos/Desktop/apps/running/Proyecto-FmarcosDevBlogCV/src/pages/tags/index.jsx"))),
-  "component---src-templates-post-post-jsx": hot(preferDefault(require("/Users/fmarcos/Desktop/apps/running/Proyecto-FmarcosDevBlogCV/src/templates/post/post.jsx"))),
-  "component---src-templates-tags-index-jsx": hot(preferDefault(require("/Users/fmarcos/Desktop/apps/running/Proyecto-FmarcosDevBlogCV/src/templates/tags/index.jsx")))
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/fmarcos/Desktop/apps/development/Proyecto-FmarcosDevBlogCV/.cache/caches/gatsby-plugin-offline/app-shell.js"))),
+  "component---src-pages-404-index-jsx": hot(preferDefault(require("/Users/fmarcos/Desktop/apps/development/Proyecto-FmarcosDevBlogCV/src/pages/404/index.jsx"))),
+  "component---src-pages-blog-index-jsx": hot(preferDefault(require("/Users/fmarcos/Desktop/apps/development/Proyecto-FmarcosDevBlogCV/src/pages/blog/index.jsx"))),
+  "component---src-pages-contact-index-jsx": hot(preferDefault(require("/Users/fmarcos/Desktop/apps/development/Proyecto-FmarcosDevBlogCV/src/pages/contact/index.jsx"))),
+  "component---src-pages-index-jsx": hot(preferDefault(require("/Users/fmarcos/Desktop/apps/development/Proyecto-FmarcosDevBlogCV/src/pages/index.jsx"))),
+  "component---src-pages-portfolio-index-jsx": hot(preferDefault(require("/Users/fmarcos/Desktop/apps/development/Proyecto-FmarcosDevBlogCV/src/pages/portfolio/index.jsx"))),
+  "component---src-pages-resume-jsx": hot(preferDefault(require("/Users/fmarcos/Desktop/apps/development/Proyecto-FmarcosDevBlogCV/src/pages/resume.jsx"))),
+  "component---src-pages-tags-index-jsx": hot(preferDefault(require("/Users/fmarcos/Desktop/apps/development/Proyecto-FmarcosDevBlogCV/src/pages/tags/index.jsx"))),
+  "component---src-templates-post-post-jsx": hot(preferDefault(require("/Users/fmarcos/Desktop/apps/development/Proyecto-FmarcosDevBlogCV/src/templates/post/post.jsx"))),
+  "component---src-templates-tags-index-jsx": hot(preferDefault(require("/Users/fmarcos/Desktop/apps/development/Proyecto-FmarcosDevBlogCV/src/templates/tags/index.jsx")))
 }
 
