8843972d3d1b34db37b5b64f248bf9e6f91bd66d
freddymhs <freddy.huaylla@klog.co>
2021-12-12 17:36:38 -0300

    mmm

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,15 +5,15 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/freddymarcoshuayllasilvestre/Desktop/Proyecto-FmarcosDevBlogCV/.cache/dev-404-page.js"))),
-  "component---src-pages-404-index-jsx": hot(preferDefault(require("/Users/freddymarcoshuayllasilvestre/Desktop/Proyecto-FmarcosDevBlogCV/src/pages/404/index.jsx"))),
-  "component---src-pages-blog-index-jsx": hot(preferDefault(require("/Users/freddymarcoshuayllasilvestre/Desktop/Proyecto-FmarcosDevBlogCV/src/pages/blog/index.jsx"))),
-  "component---src-pages-contact-index-jsx": hot(preferDefault(require("/Users/freddymarcoshuayllasilvestre/Desktop/Proyecto-FmarcosDevBlogCV/src/pages/contact/index.jsx"))),
-  "component---src-pages-index-jsx": hot(preferDefault(require("/Users/freddymarcoshuayllasilvestre/Desktop/Proyecto-FmarcosDevBlogCV/src/pages/index.jsx"))),
-  "component---src-pages-portfolio-index-jsx": hot(preferDefault(require("/Users/freddymarcoshuayllasilvestre/Desktop/Proyecto-FmarcosDevBlogCV/src/pages/portfolio/index.jsx"))),
-  "component---src-pages-resume-jsx": hot(preferDefault(require("/Users/freddymarcoshuayllasilvestre/Desktop/Proyecto-FmarcosDevBlogCV/src/pages/resume.jsx"))),
-  "component---src-pages-tags-index-jsx": hot(preferDefault(require("/Users/freddymarcoshuayllasilvestre/Desktop/Proyecto-FmarcosDevBlogCV/src/pages/tags/index.jsx"))),
-  "component---src-templates-post-post-jsx": hot(preferDefault(require("/Users/freddymarcoshuayllasilvestre/Desktop/Proyecto-FmarcosDevBlogCV/src/templates/post/post.jsx"))),
-  "component---src-templates-tags-index-jsx": hot(preferDefault(require("/Users/freddymarcoshuayllasilvestre/Desktop/Proyecto-FmarcosDevBlogCV/src/templates/tags/index.jsx")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/fmarcosdev/Downloads/Proyecto-FmarcosDevBlogCV/.cache/dev-404-page.js"))),
+  "component---src-pages-404-index-jsx": hot(preferDefault(require("/Users/fmarcosdev/Downloads/Proyecto-FmarcosDevBlogCV/src/pages/404/index.jsx"))),
+  "component---src-pages-blog-index-jsx": hot(preferDefault(require("/Users/fmarcosdev/Downloads/Proyecto-FmarcosDevBlogCV/src/pages/blog/index.jsx"))),
+  "component---src-pages-contact-index-jsx": hot(preferDefault(require("/Users/fmarcosdev/Downloads/Proyecto-FmarcosDevBlogCV/src/pages/contact/index.jsx"))),
+  "component---src-pages-index-jsx": hot(preferDefault(require("/Users/fmarcosdev/Downloads/Proyecto-FmarcosDevBlogCV/src/pages/index.jsx"))),
+  "component---src-pages-portfolio-index-jsx": hot(preferDefault(require("/Users/fmarcosdev/Downloads/Proyecto-FmarcosDevBlogCV/src/pages/portfolio/index.jsx"))),
+  "component---src-pages-resume-jsx": hot(preferDefault(require("/Users/fmarcosdev/Downloads/Proyecto-FmarcosDevBlogCV/src/pages/resume.jsx"))),
+  "component---src-pages-tags-index-jsx": hot(preferDefault(require("/Users/fmarcosdev/Downloads/Proyecto-FmarcosDevBlogCV/src/pages/tags/index.jsx"))),
+  "component---src-templates-post-post-jsx": hot(preferDefault(require("/Users/fmarcosdev/Downloads/Proyecto-FmarcosDevBlogCV/src/templates/post/post.jsx"))),
+  "component---src-templates-tags-index-jsx": hot(preferDefault(require("/Users/fmarcosdev/Downloads/Proyecto-FmarcosDevBlogCV/src/templates/tags/index.jsx")))
 }
 
