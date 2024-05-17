29d5991b26cceac1c4bf12f89dcc809ebe23a698
Freddy <fmarcosdev@gmail.com>
2024-05-17 17:27:55 -0400

    works again

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,17 +1,17 @@
 var plugins = [{
-      plugin: require('/Users/fmarcosdev/Downloads/Proyecto-FmarcosDevBlogCV/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('/home/fmarcosdev/Documents/github/Proyecto-FmarcosDevBlogCV/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('/Users/fmarcosdev/Downloads/Proyecto-FmarcosDevBlogCV/node_modules/gatsby-plugin-offline/gatsby-ssr'),
+      plugin: require('/home/fmarcosdev/Documents/github/Proyecto-FmarcosDevBlogCV/node_modules/gatsby-plugin-offline/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('/Users/fmarcosdev/Downloads/Proyecto-FmarcosDevBlogCV/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      plugin: require('/home/fmarcosdev/Documents/github/Proyecto-FmarcosDevBlogCV/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"Freddy Marcos Huaylla Silvestre","short_name":"FmarcosDev","start_url":"/","background_color":"#00BCD4","theme_color":"#00BCD4","display":"standalone","icon":"src/images/icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"6e818cc64559f7e2ef2f8c3989fe9151"},
     },{
-      plugin: require('/Users/fmarcosdev/Downloads/Proyecto-FmarcosDevBlogCV/node_modules/gatsby-plugin-sitemap/gatsby-ssr'),
+      plugin: require('/home/fmarcosdev/Documents/github/Proyecto-FmarcosDevBlogCV/node_modules/gatsby-plugin-sitemap/gatsby-ssr'),
       options: {"plugins":[],"output":"/sitemap.xml","createLinkInHead":true},
     },{
-      plugin: require('/Users/fmarcosdev/Downloads/Proyecto-FmarcosDevBlogCV/node_modules/gatsby-plugin-google-analytics/gatsby-ssr'),
+      plugin: require('/home/fmarcosdev/Documents/github/Proyecto-FmarcosDevBlogCV/node_modules/gatsby-plugin-google-analytics/gatsby-ssr'),
       options: {"plugins":[],"trackingId":"G-9FL5R42WJ0","head":false,"anonymize":false,"respectDNT":false,"exclude":[],"pageTransitionDelay":0},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
